9526b138a24c3259cc70e739eb7cef88294c5442
gy0109 <1974326896@qq.com>
2019-04-10 14:58:56 +0800

    '读取数据库'

--- mblog/mainsite/views.py
+++ mblog/mainsite/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Post
 # Create your views here.
+
+
+def homepage(request):
+    """获取所有文章 并通过django传递到客户端"""
+    posts = Post.objects.all()
+    post_lists = []
+    for count, post in enumerate(posts):
+        post_lists.append('No: {}'. format(str(count)) + str(post) + '<br>')
+
+    return HttpResponse(post_lists)
